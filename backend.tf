cf69314488b6438669fa37c91f9cd2130b1e6e4e
mikkomhirvonen <mikko.hirvonen@gofore.com>
2020-06-16 17:53:02 +0300

    -

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend s3 {
-    # Replace this with your bucket name!
     bucket         = "ex3-tf-state"
     key            = "VM/terraform.tfstate"
     region         = "us-east-2"
-    # Replace this with your DynamoDB table name!
     dynamodb_table = "ex3-tf-state"
     encrypt        = true
   }
