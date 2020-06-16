6112693573a73b3372fd616c1ebe5f5b809fe000
mikko-m-hirvonen <66619628+mikko-m-hirvonen@users.noreply.github.com>
2020-06-16 17:41:44 +0300

    Added kubernetes provider

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,10 @@
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
