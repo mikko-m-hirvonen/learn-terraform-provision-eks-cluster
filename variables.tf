629bd8be11482106cafbfe72037da12fa82e9289
mikko-m-hirvonen <66619628+mikko-m-hirvonen@users.noreply.github.com>
2020-06-16 22:44:27 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+data "aws_secretsmanager_secret_version" "my-role" {
+  secret_id = "${data.aws_secretsmanager_secret.my-role.id}"
+}
+
 variable "map_roles" {
   description = "Additional IAM roles to add to the aws-auth configmap."
   type = list(object({
@@ -8,7 +12,7 @@ variable "map_roles" {
 
   default = [
     {
-      rolearn  = ${{ secrets.MY_ROLE }}
+      rolearn  = data.aws_secretsmanager_secret_version.my-role.secret_string
       username = "KingMikko"
       groups   = ["system:masters"]
     },
